3fee8afd1a1d5905dab4d1f381ebda36e69ed6ff
kouuch <rifkyirsada2@gmail.com>
2024-12-24 17:12:00 +0800

    API_IMG_URL

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_KEY = "33302caa4b9d1494b26ae2686ee8dd30"
 
 let page = 1;
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
+const API_IMG_URL  = "https://image.tmdb.org/t/p/w1280"
 
 async function getMovies(url){
     const respon = await fetch (url)
@@ -14,7 +15,9 @@ function showMovies (movies){
     movies.forEach(movie => {
         const {title, poster_path, overview}= movie //data tertampung
         const moviesCard = document.createElement("div")
-        moviesCard.innerHTML = `<p>${title}</p>`
+        moviesCard.innerHTML = `
+        <img src="${poster_path}">
+        <p>${title}</p>`
         moviesEl.appendChild(moviesCard)
     });
 }
