cca27538db49652ab69bac36793ec20aa6064845
kouuch <rifkyirsada2@gmail.com>
2024-12-24 16:31:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const API_KEY = "33302caa4b9d1494b26ae2686ee8dd30"
+
+let page = 1;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
+
+async function getMovies(url){
+    const respon = await fetch (url)
+    const data = await respon.json()
+    console.log(data.results)
+}
