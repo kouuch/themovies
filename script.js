cea4e7d4d122f0a0922ec9d0b26067a2e3374277
kouuch <rifkyirsada2@gmail.com>
2024-12-24 17:05:49 +0800

    tresing

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ async function getMovies(url){
 function showMovies (movies){
     //document.getElementById("movies")
     moviesEl.innerHTML = ''
-    //console.log("from data movies:", movies) //tresing
     movies.forEach(movie => {
         //console.log(movie.title) //proses mapping
         const {title, poster_path, overview}= movie //data tertampung
