3cc58a597eb8516adf4553d4bae97921e92ccfb0
kouuch <rifkyirsada2@gmail.com>
2024-12-24 17:06:13 +0800

    document.getElementById("movies")

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&
 async function getMovies(url){
     const respon = await fetch (url)
     const data = await respon.json()
-    showMovies(data.results)//showMovies di tangkap -> function showMovies
+    showMovies(data.results)
 }
 
 function showMovies (movies){
-    //document.getElementById("movies")
     moviesEl.innerHTML = ''
     movies.forEach(movie => {
         //console.log(movie.title) //proses mapping
