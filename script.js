531d84c104a36462fd4e445f6f158cca6c97c7e4
kouuch <rifkyirsada2@gmail.com>
2024-12-24 18:10:06 +0800

    API_SEARCH_UR

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const API_KEY = "33302caa4b9d1494b26ae2686ee8dd30"
 let page = 1;
 const API_URL = () =>`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
 const API_IMG_URL  = "https://image.tmdb.org/t/p/w1280"
-
+const API_SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
 async function getMovies(url){
     const respon = await fetch (url)
     const data = await respon.json()
